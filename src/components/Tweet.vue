f8177969714a382650118e321f4f6c700a9b0fc8
Darshan <brunodarshan@gmail.com>
2018-05-29 20:26:36 -0300

    add moments and change dev dependencies

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="tweet">
-    <span>
-      <strong>
-        @{{ user.screen_name }} -
-      </strong>
-      {{user.name}} ·
-    </span>
+    <span> <strong> @{{ user.screen_name }} - </strong> {{user.name}}s </span>
+    <small>{{ fromNow }}</small>
     <p>{{ text }}</p>
   </div>
 </template>
 
 <script>
+import moment from 'moment'
 export default {
   name: 'Tweet',
   props: {"twt": {type: Object} },
   data: function(){
     return {
       text: this.twt.text,
-      user: this.twt.user
+      user: this.twt.user,
+      created: this.twt.created_at
+    }
+  },
+  computed: {
+    fromNow: function(){
+      return moment(this.created).fromNow();
     }
   }
 }
